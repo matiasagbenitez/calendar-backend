cd1dd5a51ae01243da513597abc63573b85c8e41
Matías Agustín Benítez <matiasagbenitez@gmail.com>
2023-07-13 16:11:13 -0300

    registro de usuario con encriptación de contraseña

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,45 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs');
+const Usuario = require('../models/Usuario');
 
-const crearUsuario = (req, res = response) => {
-    const { name, email, password } = req.body;
-    res.status(201).json({
-        ok: true,
-        msg: 'register',
-        name,
-        email,
-        password,
-    });
+const crearUsuario = async (req, res = response) => {
+    
+    const { email, password } = req.body;
+
+    try {
+
+        // Verificar que el email no exista
+        let usuario = await Usuario.findOne({ email });
+        if (usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario ya existe con ese email.'
+            });
+        }
+
+        // Crear usuario con el modelo
+        usuario = new Usuario(req.body);
+
+        // Encriptar contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt);
+
+        // Guardar usuario en la base de datos
+        await usuario.save();
+
+        // Devolver respuesta exitosa
+        res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: 'Error interno del servidor.'
+        });
+    }
 }
 
 const loginUsuario = (req, res = response) => {
